74d59c01d2911b8f79a29d2a65052a687eedd137
PauloCzarnewski <paulo.czarnewski@ki.se>
2022-05-22 11:27:15 +0200

    adds config for neovim

--- install/03_simlinks.sh
+++ install/03_simlinks.sh
@@ -9,5 +9,9 @@ ln -sfn ~/.dotfiles/vim ~/.vim
 ln -sfn ~/.vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/vim/colors/* /home/ubuntu/opt/vim/share/vim/vim82/color
 
+# Neovim
+mkdir -d ~/.config/nvim/init.vim
+ln -sfn ~/.dotfiles/neovim/init.vim ~/.config/nvim/init.vim
+
 # Tmux
 ln -sfn ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
--- install/install_neovim.sh
+++ install/install_neovim.sh
@@ -3,8 +3,22 @@
 curl -o neovim_source.tar.gz
 https://codeload.github.com/neovim/neovim/tar.gz/refs/tags/stable
 
+alias targz='tar -czvf'
+alias untargz='tar -xvzf'
+
 untargz neovim_source.tar.gz
 cd neovim-0.5.1
 
 make CMAKE_INSTALL_PREFIX=~/opt/neovim
+make install
+
+
+cd..
+rm -rf neovim-stable
+
+mkdir -d ~/.config
+ln -sfn ~/.dotfiles/nvim ~/.config/nvim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
